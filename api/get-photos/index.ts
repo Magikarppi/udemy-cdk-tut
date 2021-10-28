8200609f02a22b4db6add1b4087cf1284ec7499e
Sami H <samih90@gmail.com>
2021-10-28 23:00:54 +0300

    lambda listing s3 objects and url generation

--- api/get-photos/index.ts
+++ api/get-photos/index.ts
@@ -3,19 +3,53 @@ import {
   Context,
   APIGatewayProxyResultV2,
 } from 'aws-lambda';
+import { S3 } from 'aws-sdk';
+
+const bucketName = process.env.PHOTO_BUCKET_NAME;
+const s3 = new S3();
+
+async function generateUrl(
+  object: S3.Object
+): Promise<{ filename: string; url: string }> {
+  const url = await s3.getSignedUrlPromise('getObject', {
+    bucketName,
+    Key: object.Key,
+    // Expires: (24 * 60 * 60)
+  });
+  return {
+    filename: object.Key!,
+    url,
+  };
+}
 
 async function getPhotos(
   event: APIGatewayProxyEventV2,
   context: Context
 ): Promise<APIGatewayProxyResultV2> {
+  console.log('bucketName:', bucketName);
+  if (!bucketName) {
+    throw new Error('env var PHOTO_BUCKET_NAME invalid');
+  }
   try {
+    const { Contents: results } = await s3
+      .listObjectsV2({ Bucket: bucketName })
+      .promise();
+    if (results) {
+      const photos = await Promise.all(
+        results.map((result) => generateUrl(result))
+      );
+      return {
+        statusCode: 200,
+        body: JSON.stringify(photos),
+      };
+    }
     return {
       statusCode: 200,
-      body: 'Hello from lambda',
+      body: 'Nothing to list',
     };
   } catch (error) {
     return {
-      statusCode: 404,
+      statusCode: 500,
       body: `${error}`,
     };
   }
--- lib/udemy-cdk-tut-stack.ts
+++ lib/udemy-cdk-tut-stack.ts
@@ -1,6 +1,7 @@
 import { Runtime } from '@aws-cdk/aws-lambda';
 import { NodejsFunction } from '@aws-cdk/aws-lambda-nodejs';
 import { Bucket, BucketEncryption } from '@aws-cdk/aws-s3';
+import { BucketDeployment, Source } from '@aws-cdk/aws-s3-deployment';
 import * as cdk from '@aws-cdk/core';
 import { CfnOutput } from '@aws-cdk/core';
 import * as path from 'path';
@@ -13,10 +14,18 @@ export class UdemyCdkTutStack extends cdk.Stack {
       encryption: BucketEncryption.S3_MANAGED,
     });
 
+    new BucketDeployment(this, 'UdemyCdkTutPhotosBucketDeployment', {
+      sources: [Source.asset(path.join(__dirname, '..', 'photos'))],
+      destinationBucket: bucket,
+    });
+
     const getPhotos = new NodejsFunction(this, 'GetPhotosLambda', {
       handler: 'getPhotos',
       runtime: Runtime.NODEJS_14_X,
       entry: path.join(__dirname, '..', 'api', 'get-photos', 'index.ts'),
+      environment: {
+        PHOTO_BUCKET_NAME: bucket.bucketName,
+      },
     });
 
     new CfnOutput(this, 'UdemyCdkTutBucketNameExport', {
